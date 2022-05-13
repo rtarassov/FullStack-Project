90c40bbe2d83aff7a5d9aec97ce288ea5d3af594
richard.tarassov <richard.tarassov@hotmail.com>
2022-05-13 08:28:54 +0300

    Initial commit

---
+++ src/main/java/tarassov/project/FullStackProjectBackendApplication.java
@@ -0,0 +1,13 @@
+package tarassov.project;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FullStackProjectBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FullStackProjectBackendApplication.class, args);
+    }
+
+}
---
+++ src/test/java/tarassov/project/FullStackProjectBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package tarassov.project;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FullStackProjectBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
